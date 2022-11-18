b5cb315518f2bf3ce01b24ebd5ff2698484d1924
Md Obaydul Hoque <84621516+odht82@users.noreply.github.com>
2022-11-18 22:44:00 +0600

    styles update

---
+++ src/Components/Row.js
@@ -0,0 +1,15 @@
+import Column from './Column';
+
+const Row = ({ className, content }) => {
+    return (
+        <div className={className}>
+            {
+                content.map(column => (
+                    <Column key={column.id} className={column.id} content={column.content} />
+                ))
+            }
+        </div>
+    );
+};
+
+export default Row;
