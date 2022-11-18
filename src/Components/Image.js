252a583568f8b8d9236cdec41df48adff7cc87d6
Md Obaydul Hoque <84621516+odht82@users.noreply.github.com>
2022-11-18 22:42:46 +0600

    Delete src directory

--- src/Components/Image.js
+++
@@ -1,9 +0,0 @@
-const Image = ({ className, attr }) => {
-
-    return (
-        <>
-            <img className={className} src={attr.src} alt={attr.alt}></img>
-        </>
-    );
-};
-export default Image;
--- src/Utilities/StyleSetter.js
+++
@@ -1,7 +0,0 @@
-let Style = document.createElement('style');
-document.getElementsByTagName('head')[0].appendChild(Style)
-export const setStyle = (styleStr) => {
-    if (typeof styleStr === 'string') {
-        Style.appendChild(document.createTextNode(styleStr))
-    }
-}
